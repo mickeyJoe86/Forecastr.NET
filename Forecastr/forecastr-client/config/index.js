0c3f97dfb8693d45b2a635aaadca57c0205033c9
Mike Ward <ward.michael51@gmail.com>
2017-10-29 13:09:45 -0400

    updates to index.html locale

--- Forecastr/forecastr-client/config/index.js
+++ Forecastr/forecastr-client/config/index.js
@@ -9,9 +9,9 @@ module.exports = {
   build: {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../../index.html'),
-    assetsRoot: path.resolve(__dirname, '../../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/forecastr-client/dist',
+    assetsRoot: path.resolve(__dirname, '../..'),
+    assetsSubDirectory: 'public',
+    assetsPublicPath: '',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
