b9738943483b0f3ff336562b044c031516f58779
Mike Ward <ward.michael51@gmail.com>
2017-10-29 13:50:26 -0400

    Added unit test framework wuth Xunit

---
+++ Forecastr/Models/HomeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Forecastr.Models
+{
+    public class HomeModel
+    {
+        public HomeModel(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ForecastrTests/Models/HomeModelTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Forecastr.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forecastr.Models.Tests
+{
+    public class HomeModelTests
+    {
+        [Fact()]
+        public void HomeModelTest()
+        {
+            Assert.True(true, "This test needs an implementation");
+        }
+    }
+}
